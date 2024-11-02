37c4d4caca81f78a84f906665f006be89622decc
Raman Soni <69788442+RmnSoni@users.noreply.github.com>
2024-11-02 23:34:45 +0530

    updated date format and margins

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -24,6 +24,12 @@ interface BlogPostProps {
         slug: string;
     }>;
 }
+const dateOptions = {
+    weekday:"long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
 export default function BlogPost({params}: BlogPostProps){
 
     const {slug} = React.use(params)
@@ -33,9 +39,9 @@ export default function BlogPost({params}: BlogPostProps){
         <div className='p-6'>
             <div className='text-center' >
             <h1 className=' text-3xl text-slate-800 dark:text-teal-500 font-bold'>{post.data.title}</h1>
-            <p className='text-slate-500'>{post.data.date.toDateString()}</p>
+            <p className='text-slate-500'>{post.data.date.toLocaleDateString("en-US", dateOptions)}</p>
             </div>
-            <article className='text-lg prose max-w-none md:prose-lg prose-slate dark:prose-invert my-2'>
+            <article className='text-lg prose max-w-none md:prose-lg prose-slate dark:prose-invert my-8'>
                 <Markdown>
                     {post.content}
                 </Markdown>
